6565597b4f68562e3ccbdca84f94fea3c6b456c9
krk Kola <ramakrishnasfd1993@gmail.com>
2025-12-02 15:16:14 +0000

    updated with new code

--- models/marts/markettng/student_pass_status.sql
+++ models/marts/markettng/student_pass_status.sql
@@ -3,5 +3,9 @@ select
     student_name,
     branch_name,
     pass_percentage,
-    case when pass_percentage >= 35 then 'Pass' else 'Fail' end as pass_status
+    case 
+    when pass_percentage >= 80 then 'excellent' 
+    when pass_percentage >= 60 then 'good'
+    when pass_percentage >= 40 then 'pass'
+    else 'Fail' end as pass_status
 from {{ ref("students") }}
--- tests/total_avg_marks_of_students.sql
+++ tests/total_avg_marks_of_students.sql
@@ -1,1 +1,1 @@
-select * from {{ ref("student_pass_status") }} where pass_status = 'Fail'
+select * from {{ ref("student_pass_status") }} where pass_status = 'good'
