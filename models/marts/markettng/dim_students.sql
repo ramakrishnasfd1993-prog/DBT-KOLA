d5b20ec168d52331be3f983c97f39319343d61d8
krk Kola <ramakrishnasfd1993@gmail.com>
2025-12-24 13:16:55 +0000

    changes cimmited

--- models/marts/markettng/dim_students.sql
+++ models/marts/markettng/dim_students.sql
@@ -14,12 +14,12 @@ with
             s.branch_name as branch_code,  -- use whatever column exists in your students model
             b.branch_name as branch_fullname,
             s.college_name,
-            c.collge_name as collge_name,  -- kept the typo to match your raw table
+            c.college_name as collge_name,  -- kept the typo to match your raw table
             c.college_id,
             s.pass_percentage
         from students s
         left join branches b on upper(trim(s.branch_name)) = upper(trim(b.branch_id))
-        left join colleges c on upper(trim(s.college_name)) = upper(trim(c.collge_name))
+        left join colleges c on upper(trim(s.college_name)) = upper(trim(c.college_name))
 
     )
 
--- models/stagging/jaffle_shop/college.sql
+++ models/stagging/jaffle_shop/college.sql
@@ -1,5 +1,5 @@
 -- models/staging/stg_college.sql
 select
-    collge_name,  -- matches your DDL
+    college_name,  -- matches your DDL
     college_id
 from {{ source('jaffle_shop', 'college') }}
